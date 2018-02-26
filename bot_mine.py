67d011260f32f5bb2e41372719bbc8c75ccddd75
Jose <j.albertorodriguez89@gmail.com>
2018-02-26 23:07:23 +1030

    Added code test

--- bot_mine.py
+++ bot_mine.py
@@ -1,4 +1,5 @@
-from telepot import Bot
+from telepot import Bot, DelegatorBot
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 import telepot
 import json
 from telepot.loop import MessageLoop
@@ -8,7 +9,14 @@ except ImportError as e:
     print(e)
 
 
-class MyBot(Bot):
+class MyBot(telepot.Bot):
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Temperature', callback_data='temp')],
+        [InlineKeyboardButton(text='Humidity', callback_data='hum')],
+        [InlineKeyboardButton(text='Photo', callback_data='photo')],
+
+    ])
 
     def __init__(self, settings_file='settings.json'):
         print('Starting Bot...')
@@ -18,10 +26,14 @@ class MyBot(Bot):
         try:
             self.sense = Sense()
             self.message(self.handle_sense)
+            MessageLoop(self, {'chat': self.handle,
+                               'callback_query': self.on_callback_query}).run_as_thread()
         except Exception as e:
             print(e)
-            print('Not in Raspberry')
+            print('Not in Raspberry or did not detect sense Hat')
+            # MessageLoop(self)
             self.message(self.handle)
+
 
     @staticmethod
     def get_settings(file):
@@ -39,7 +51,29 @@ class MyBot(Bot):
 
         if content_type == 'text':
             print(msg['text'])
-            self.sendMessage(chat_id, msg['text'])
+            #self.sendMessage(chat_id, msg['text'])
+        self.sendMessage(self.user_id, 'Actions', reply_markup=self.keyboard)
+
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        print(content_type, chat_type, chat_id)
+
+        if content_type == 'text':
+            print(msg['text'])
+            # self.sendMessage(chat_id, msg['text'])
+        self.sendMessage(self.user_id, 'Actions', reply_markup=self.keyboard)
+
+    def on_callback_query(self, msg):
+        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+        print('Callback Query:', query_id, from_id, query_data)
+        if query_data == 'temp':
+            self.answerCallbackQuery(query_id, text='Getting Temp')
+            self.sendMessage(self.user_id, text=str(self.sense.temperature()))
+        elif query_data == 'hum':
+            self.answerCallbackQuery(query_id, text='Getting Humidity')
+            self.sendMessage(self.user_id, text=self.sense.humidity())
+        else:
+            self.answerCallbackQuery(query_id, text='Got it')
 
     def handle_sense(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
